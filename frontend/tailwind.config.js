16e78cdbc893fb82ff94e8c7bc82b106e7da6f7d
yaakoub <yaakoub.leguennec@gmail.com>
2026-02-26 19:27:34 +0100

    Initialisation frontend, ajout de nextAuth, daisyUI, lucide. Les polices et les themes daisyUI.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-roboto)"],
+        poppins: ["var(--font-poppins)"],
+        inter: ["var(--font-inter)"]
+      },
+    },
+  },
+};
